9b55114685d47fef69c0f3023a044c86daf21bd3
miftah1299 <miftahjannat25@gmail.com>
2025-01-15 00:58:43 +0600

    Add initial implementation of various packages and modules for the project

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def handle_set_username(username):
     emit('update_user_list', list(users.values()), broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
